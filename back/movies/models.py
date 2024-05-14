39d959ed9e71b3a0adf01c7cd5a5707c3f6e8d06
growbori <grow.boribori@gmail.com>
2024-05-14 16:09:08 +0900

    django movies models 생성

--- back/movies/models.py
+++ back/movies/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Actor(models.Model):
+  name = models.TextField()
+  filmo = models.TextField()
+  
+class Director(models.Model):
+  name = models.TextField()
+  
+class Genre(models.Model):
+  genre = models.TextField()
+  
+class Movie(models.Model):
+  id = models.IntegerField()
+  title = models.CharField(max_length = 30)
+  score = models.FloatField()
+  overview = models.TextField()
+  popularity = models.FloatField()
+  poster = models.ImageField()
+  release_date = models.DateField()
+  movieCd = models.IntegerField()
+  actors = models.ManyToManyField(Actor)
+  genre = models.TextField(Genre)
+  directors = models.ManyToManyField(Director)
+
+  
