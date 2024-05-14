9d6d0bb2ffcbd0ca61651cf218676b61c428ba18
탁호준 <poppop1818@naver.com>
2024-05-14 16:42:27 +0900

    django accounts모델 오류 해결 Movie 모델의 id로 인한 오류 해결

--- back/movie_project/settings.py
+++ back/movie_project/settings.py
@@ -140,3 +140,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+#accounts 앱의 User 클래스
--- back/movies/models.py
+++ back/movies/models.py
@@ -3,22 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Actor(models.Model):
-  name = models.TextField()
+  actor_name = models.TextField()
   filmo = models.TextField()
   
 class Director(models.Model):
-  name = models.TextField()
+  director_name = models.TextField()
   
 class Genre(models.Model):
   genre = models.TextField()
   
 class Movie(models.Model):
-  id = models.IntegerField()
+  movie_id = models.IntegerField()
   title = models.CharField(max_length = 30)
   score = models.FloatField()
   overview = models.TextField()
   popularity = models.FloatField()
-  poster = models.ImageField()
+  poster_url = models.TextField()
   release_date = models.DateField()
   movieCd = models.IntegerField()
   actors = models.ManyToManyField(Actor)
